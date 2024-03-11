6ebaadb865ed6312b251f62b4925e87b96754c26
Aline <alinemariana1@hotmail.com>
2024-03-11 13:38:14 -0300

    inicializando api rest

---
+++ src/main/java/com/alinembs/imageapi/images/ImagensController.java
@@ -0,0 +1,22 @@
+package com.alinembs.imageapi.images;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/images")
+public class ImagensController {
+
+	
+//	public ResponseEntity  save()
+//	{
+//		
+//		
+//	}
+//	
+	
+	
+	
+	
+}
---
+++ src/test/java/com/alinembs/imageapi/ImageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinembs.imageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
